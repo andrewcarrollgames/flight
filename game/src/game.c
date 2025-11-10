0bc37c852ae4b47820f2c0129cb049c99e41ed7a
andrewcarrollgames <andrewcarrollgames@gmail.com>
2025-11-09 19:19:57 -0600

    Preparation for hot reloading
    *Added cr as a submodule to engine/extern
    *Cleaned up some dependencies in cmake
    *Added anticipated API pointers that will be required to allow game to call into platform/engine code

--- game/src/game.c
+++ game/src/game.c
@@ -1,20 +1,19 @@
 #include "game.h"
 #include "game_state.h"
-#include "platform.h"
 
 bool Game_Initialize(GameState *gameState) {
-  Platform_Log("Game Initializing.");
+  //Platform_Log("Game Initializing.");
   gameState->isRunning = true;
   return true;
 }
 
 void Game_Update(GameState *gameState) {
   if (gameState->isRunning) {
-    Platform_Log("Game Updating.");
+    //Platform_Log("Game Updating.");
   }
 }
 
 void Game_Shutdown(GameState *gameState) {
-  Platform_Log("Game Shutting Down.");
+  //Platform_Log("Game Shutting Down.");
   gameState->isRunning = false;
 }
